f337758b2a82bee2de0d5e0ae81023e21e7bed30
mingkiii <kimchunjae92@gmail.com>
2023-07-12 20:56:58 +0900

    Initial commit

---
+++ src/main/java/com/example/translationchat/TranslationchatApplication.java
@@ -0,0 +1,13 @@
+package com.example.translationchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationchatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationchatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/translationchat/TranslationchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.translationchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslationchatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
