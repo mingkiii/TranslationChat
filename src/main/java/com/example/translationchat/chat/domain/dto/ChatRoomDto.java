8e9b0b09815247604479f08f33701a53f194890f
mingkiii <kimchunjae92@gmail.com>
2023-08-09 03:02:13 +0900

    feature : 대화방 나가기, 대화방 목록 조회

---
+++ src/main/java/com/example/translationchat/chat/domain/dto/ChatRoomDto.java
@@ -0,0 +1,25 @@
+package com.example.translationchat.chat.domain.dto;
+
+import com.example.translationchat.chat.domain.model.ChatRoom;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ChatRoomDto {
+    private Long id;
+    private String title;
+
+    public static ChatRoomDto from(ChatRoom chatRoom) {
+        return ChatRoomDto.builder()
+            .id(chatRoom.getId())
+            .title(chatRoom.getTitle())
+            .build();
+    }
+}
